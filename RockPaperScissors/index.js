18c760f795540bb29cb884a280987f37b8bbe610
XAIMOH001 <xaimohchau@gmail.com>
2026-04-25 02:15:12 +0300

    updated the game using Arrays

--- RockPaperScissors/index.js
+++ RockPaperScissors/index.js
@@ -1,42 +1,39 @@
-let playGame = confirm("shall we play rock, paper, scissors?");
+let playGame = confirm("shall we play rock paper scissors?");
 
 if (playGame) {
-  while (playGame) {
-    const playerChoice = prompt("please enter 'rock', 'paper', or 'scissors'");
-    if (playerChoice || playerChoice === "") {
-      const firstPlayer = playerChoice.trim().toLowerCase();
-      if (
-        firstPlayer === "rock" ||
-        firstPlayer === "paper" ||
-        firstPlayer === "scissors"
-      ) {
-        const computerChoice = Math.floor(Math.random() * 3);
-        const choiceArray = ["rock", "paper", "scissors"];
-        const computer = choiceArray[computerChoice];
+    //play
+    while (playGame) {
+        const playerChoice = prompt("Please enter 'rock' 'paper' or 'scissors': ");
+        if (playerChoice || playerChoice === "") {
+            const firstPlayer = playerChoice.trim().toLowerCase();
+            if (firstPlayer === "rock" || firstPlayer === "paper" || firstPlayer === "scissors") {
+                const choices = ["rock", "paper", "scissors"];
+                const computerChoice = Math.floor(Math.random() * 3);
+                const computer = choices[computerChoice];
 
-        const result =
-          firstPlayer === computer
-            ? "Tie game"
-            : firstPlayer === "rock" && computer === "paper"
-              ? `firstPlayer: ${firstPlayer} \nComputer: ${computer} \nComputer Wins`
-              : firstPlayer === "paper" && computer === "scissors"
-                ? `firstPlayer: ${firstPlayer} \nComputer: ${computer} \nComputer Wins`
-                : firstPlayer === "scissors" && computer === "rock"
-                  ? `firstPlayer: ${firstPlayer} \nComputer: ${computer} \nComputer Wins`
-                            : `firstPlayer: ${firstPlayer} \nComputer: ${computer} \firstPlayer Wins`;
-          alert(result);
-          playGame = confirm("play Again");
-          if (!playGame) alert("Ok, thanks for playing.");
-          continue;
-      } else {
-          alert("you din't enter rock, paper, or scissors.");
-          continue;
-      }
-    } else {
-        alert("I guess you changed your mind. Maybe next time.");
-        break;
+                const result =
+                    firstPlayer === computer
+                    ? "Tie game"
+                    : firstPlayer === "rock" && computer === "paper"
+                    ? `firstPlayer: ${firstPlayer} \nComputer: ${computer} \nComputer Wins!`
+                    : firstPlayer === "paper" && computer === "scissors"
+                    ? `firstPlayer: ${firstPlayer} \nComputer: ${computer} \nComputer Wins!`
+                    : firstPlayer === "scissors" && computer === "rock"
+                    ? `firstPlayer: ${firstPlayer} \nComputer: ${computer} \nComputer Wins!`
+                    : `firstPlayer: ${firstPlayer} \nComputer: ${computer} \nfirstPlayer Wins!`;
+                alert(result);
+                playGame = confirm("Play Again");
+                if (!playGame) alert("Okay, thanks for playing.");
+                continue;
+            } else {
+                alert("You did not enter 'rock' 'paper' or 'scissors'");
+                continue;
+            }
+        } else {
+            alert("Looks like you changed your mind. Maybe next time.");
+            break;
+        }
     }
-  }
 } else {
-  alert("maybe next time");
+    alert("Maybe next Time");
 }
